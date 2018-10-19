c4905d3faa3dccb0643a80f0675f15f7a8ff4f3d
ondrejkrejci <krejcio@fzu.cz>
2018-10-19 11:52:08 +0300

    PPSTM_simple.py went through 1st tests

--- tests/4N-coronene/clean.sh
+++ tests/4N-coronene/clean.sh
@@ -5,6 +5,12 @@ rm -r ./Q0.00K0.50
 rm *.npy
 echo "Removing calculated Figures"
 rm didv_*.png
+echo "Removing outputs of PPSTM_simple, if any"
+rm STM_*.png
+rm didv_*tip_*WF_*eta_*.xsf
+rm STM_*tip_*WF_*eta_*.xsf
+rm didv_*tip_*WF_*eta_*.xyz
+rm STM_*tip_*WF_*eta_*.xyz
 echo "Done, bye bye"
 
 
--- tests/4N-coronene/run_test.sh
+++ tests/4N-coronene/run_test.sh
@@ -13,6 +13,6 @@ echo "test for the PP-STM code:"
 python PPSTM/PPAFM/generateLJFF.py -i crazy_mol.xyz --npy
 python PPSTM/PPAFM/relaxed_scan.py --pos --npy
 python PPSTM/PPAFM/plot_results.py --pos --df --save_df --npy
-python PPSTM/dIdV_test_4N-coronene.py
-python PPSTM/dIdV_test_4N-coronene_cp2k.py
+#python PPSTM/dIdV_test_4N-coronene.py
+#python PPSTM/dIdV_test_4N-coronene_cp2k.py
 echo "Now all things made, before submiting, please run clean.sh!"
