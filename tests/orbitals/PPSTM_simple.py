82a977aa828adf1b316806c9cd6c1a68741b5ddb
Lauri Kurki <lauri.v.kurki@gmail.com>
2024-08-28 16:38:20 +0300

    added the same fix for all tests

--- tests/orbitals/PPSTM_simple.py
+++ tests/orbitals/PPSTM_simple.py
@@ -160,6 +160,7 @@ else:
     extent = (x[0],x[1],y[0],y[1])
     tip_r  = RS.mkSpaceGrid(x[0],x[1],x[2],y[0],y[1],y[2],z[0],z[1],z[2])
     lvec   = np.array([[x[0],y[0],z[0]],[x[1]-x[0],0.,0.],[0.,y[1]-y[0],0.],[0.,0.,z[1]-z[0]]])
+    atomic_info_or_head = (None, None)
     #print "DEBUG: extent", extent
     #print "DEBUG: lvec", lvec
     tip_r0 = tip_r
