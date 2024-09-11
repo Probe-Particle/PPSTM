8cc9b41f7380bb0ae564560abcb60b21ee2bf858
Lauri Kurki <lauri.v.kurki@gmail.com>
2024-09-11 10:55:12 +0300

    same fix for all test scripts

--- tests/4N-coronene/PPSTM_simple_pxy.py
+++ tests/4N-coronene/PPSTM_simple_pxy.py
@@ -188,7 +188,7 @@ else:
                            y[1], y[2], z[0], z[1], z[2])
     lvec = np.array([[x[0], y[0], z[0]], [x[1]-x[0], 0., 0.],
                     [0., y[1]-y[0], 0.], [0., 0., z[1]-z[0]]])
-    atomic_info_or_head = (None, None)
+    atomic_info_or_head = (np.zeros((4,1)), np.zeros((4,3)))
     # print "DEBUG: extent", extent
     # print "DEBUG: lvec", lvec
     tip_r0 = tip_r
