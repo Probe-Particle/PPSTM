3da0d6119b251c820e1a4f1cf7140eee27b42e55
Lauri Kurki <lauri.v.kurki@gmail.com>
2024-09-11 11:10:33 +0300

    Note for a temp fix

--- PPSTM_simple.py
+++ PPSTM_simple.py
@@ -156,7 +156,9 @@ else:
     extent = (x[0],x[1],y[0],y[1])
     tip_r  = RS.mkSpaceGrid(x[0],x[1],x[2],y[0],y[1],y[2],z[0],z[1],z[2])
     lvec   = np.array([[x[0],y[0],z[0]],[x[1]-x[0],0.,0.],[0.,y[1]-y[0],0.],[0.,0.,z[1]-z[0]]])
-    atomic_info_or_head = (np.zeros((4,1)), np.zeros((4,3)))
+    # NOTE: This is a temporary fix for the atomic_info_or_head, it will be fixed in the future
+    #       by reading the atomic info from the geometry file
+    atomic_info_or_head = (np.zeros((4,1)), np.zeros((4,3))) 
     #print "DEBUG: extent", extent
     #print "DEBUG: lvec", lvec
     tip_r0 = tip_r
