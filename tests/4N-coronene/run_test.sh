f26b4b60a09f818b23d9d1a480e3d45212a060f3
Lauri Kurki <lauri.v.kurki@gmail.com>
2024-09-27 22:14:24 +0900

    4N coronene tests now use the new config file system

--- tests/4N-coronene/run_test.sh
+++ tests/4N-coronene/run_test.sh
@@ -15,11 +15,12 @@ echo "test for the PP-STM code:"
 ppafm-generate-ljff -i crazy_mol.xyz -f npy
 ppafm-relaxed-scan --pos -f npy
 ppafm-plot-results --pos --df --save_df -f npy
-python3 PPSTM/dIdV_test_4N-coronene.py
-python3 PPSTM/dIdV_test_4N-coronene_cp2k.py
+
+python3 dIdV_test_4N-coronene.py
+python3 dIdV_test_4N-coronene_cp2k.py
+python3 ../../ppstm_run.py s-relaxed-png.toml
+python3 ../../ppstm_run.py pxy-relaxed.toml
+python3 ../../ppstm_run.py s-relaxed.toml
 python3 PPdos_simple.py
-python3 PPSTM_simple.py
-python3 PPSTM_simple_pxy.py
-python3 PPSTM_simple_s.py
 python3 SUM_Orb_Contrib.py
 echo "Now all things made, before submiting, please run clean.sh!"
