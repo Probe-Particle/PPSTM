c18b3ff0c82842090756e5dc295e3c858d5aac72
Lauri Kurki <lauri.v.kurki@gmail.com>
2024-09-30 23:07:24 +0900

    updated Si_7x7 tests

--- tests/Si_7x7/run_all_tests.sh
+++ tests/Si_7x7/run_all_tests.sh
@@ -10,15 +10,15 @@ echo $OMP_NUM_THREADS
 echo "Now the tests:"
 
 echo "test for the PP-STM code ploting DOS of Si(111) 7x7 reconstruction:"
-python3 PPSTM/PDOS_test_Si_7x7.py
+python3 PDOS_test_Si_7x7.py
 echo "test for the PP-STM code on the example os Si(111) 7x7 reconstruction - single STM calc -- +- 0.5 V:"
-python3 PPSTM/dIdV_test_Si_7x7.py
+python3 dIdV_test_Si_7x7.py
 echo "Now all things made, before submiting please run clean.sh"
 echo "test for the PP-STM code on the example os Si(111) 7x7 reconstruction - full STM & dIdV Scan:"
-python3 PPSTM/STM_test_Si_7x7.py
+python3 STM_test_Si_7x7.py
 
 python3 PPdos_simple.py
-python3 PPSTM_simple.py
+python3 ../../ppstm_run.py si_7x7.toml
 echo "Now all things made, before submiting please run clean.sh"
 
 
