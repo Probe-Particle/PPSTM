7098d31fd4db7cd890bea82ef83fcc04cad81d18
Gianmarco Midena <gianmarco.midena@gmail.com>
2026-06-10 10:37:54 +0300

    fix(visualization): align XSF naming with work function decay processing

--- pyPPSTM/visualization.py
+++ pyPPSTM/visualization.py
@@ -136,7 +136,8 @@ def plot_wsxm(config, current, didv, voltages, names, tip_r0,
                 np.savetxt(f, out_curr)
                 f.close()
 
-def save_xsf(config, current, didv, voltages, names, geom_plot, lvec):
+def save_xsf(config, current, didv, voltages, names, geom_plot, lvec,
+             save_dir: Path = Path(".")):
     try:
         import ppafm.io as io
     except ImportError:
@@ -146,7 +147,11 @@ def save_xsf(config, current, didv, voltages, names, geom_plot, lvec):
     tip_orb = config['scan']['tip_orb']
     eta = config['scan']['eta']
     work_function = config['advanced']['work_function']
-    wf_decay = config['advanced']['work_function_decay']
+
+    if config['scan']['scan_type'] in ['STM', 'STM-single', 'v-scan', 'V-scan', 'Voltage-scan']:
+        wf_decay = config['advanced']['work_function_decay']
+    else:
+        wf_decay = 0.
 
     xsf_head = Bu.At2XSF(geom_plot) if geom_plot is not None else io.XSF_HEAD_DEFAULT
     nV, nH = get_number_of_voltages_and_heights(config, current, didv)
@@ -154,10 +159,10 @@ def save_xsf(config, current, didv, voltages, names, geom_plot, lvec):
     for vv in range(nV):
         if didv is not None:
             name_file = f'didv_{names[vv]}_tip_{tip_type}-{tip_orb}_WF_{work_function-voltages[vv]*wf_decay}_eta_{eta:.1f}.xsf'
-            io.saveXSF(name_file, didv[vv], lvec, head=xsf_head )
+            io.saveXSF(save_dir.joinpath(name_file), didv[vv], lvec, head=xsf_head )
         if current is not None:
             name_file = f'STM_{names[vv]}_tip_{tip_type}-{tip_orb}_WF_{work_function:.1f}_WF_decay_{wf_decay:.1f}_eta_{eta:.1f}.xsf'
-            io.saveXSF(name_file, current[vv], lvec, head=xsf_head )
+            io.saveXSF(save_dir.joinpath(name_file), current[vv], lvec, head=xsf_head )
 
 def save_npy(config, current, didv, voltages, names, lvec, atomic_info_or_head):
     try:
